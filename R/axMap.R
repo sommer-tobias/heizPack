f58b80ddf21e9f0aaadf5d19fad7f66642927964
tobias.sommer <tobias.sommer@lu.ch>
2024-06-14 07:55:24 +0200

    add documentation

--- R/axMap.R
+++ R/axMap.R
@@ -1,9 +1,25 @@
+#' Transformation based on two axis
+#'
+#' @param input The values you want to transform
+#' @param ax1 First axis limits
+#' @param ax2 Second axis limits
+#' @param direction Either 'transformAxis' or 'transformValues'
+#'
+#' @return The trnsformed vlues
+#' @export
+#'
+#' @examples
+#' direction <- 'transformAxis'
+#' input <-  c(800, 900, 1000)
+#' ax1 <- c(80,100)
+#' ax2 <- c(700, 1300)
+#' axMap(input, ax1, ax2, direction)
 axMap = function(input, ax1, ax2, direction){
 
-  # direction = 'transformAxis'
-  # input = c(800, 900, 1000)
-  # ax1 <- c(80,100)
-  # ax2 <- c(700, 1300)
+  direction <- 'transformAxis'
+  input <- c(800, 900, 1000)
+  ax1 <- c(80,100)
+  ax2 <- c(700, 1300)
 
   diffAx1 <- diff(ax1)
   diffAx2 <- diff(ax2)
